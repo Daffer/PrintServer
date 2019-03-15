436e026b9fde8daf6133ee293d04597478c01fd6
Pavel <catkiller95@gmail.com>
2019-03-15 16:23:58 +0300

    Контроллер сканнера

---
+++ app/controllers/scan.js
@@ -0,0 +1,16 @@
+const settings = require('./interfaces/iScanSettings');
+
+//==================================//
+//          SCAN CONTROLLER         //
+//==================================//
+
+class ScanController {
+    //  render  //
+    static async getSettings() {
+        return settings.getAllOptions();
+    }
+}
+
+module.exports = {
+    ScanController: ScanController
+};
